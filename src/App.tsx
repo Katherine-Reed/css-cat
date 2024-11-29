6b8715c21b6314d36db23be139e04c12b236f84c
Katherine Reed <katherine.grace.reed@gmail.com>
2024-11-29 17:17:10 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+export default function App() {
+  return (
+    <main className="cat">
+      <section className="ear ear-left"></section>
+      <section className="ear ear-right"></section>
+      <section className="face">
+        <section className="tongue"></section>
+        <section className="cheek cheek-left"></section>
+        <section className="cheek cheek-right"></section>
+      </section>
+      <section className="muzzle"></section>
+      <section className="eye eye-left">
+        <section className="eye-pupil"></section>
+      </section>
+      <section className="eye eye-right">
+        <section className="eye-pupil"></section>
+      </section>
+    </main>
+  )
+
+}
